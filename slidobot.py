fe33e2c623775d0589c3ee60dc929938819d154f
Cormuckle <cofeely@tcd.ie>
2023-04-23 15:22:36 +0100

    Removed need to download chromedriver

--- slidobot.py
+++ slidobot.py
@@ -1,21 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import time
 import sys
 import getopt
+from webdriver_manager.chrome import ChromeDriverManager
 
+EVERYONE = [2,3,5,6]
 class SlidoBot:
-    def __init__(self, hash=None, xpath=None, driver=None):
+    def __init__(self, hash=None, xpath=None, driver=None, who=1):
         self.driver = driver
-        
-        self.driver = webdriver.Chrome(self.driver)
+        self.who = who
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     def closeBrowser(self):
         self.driver.close()
 
     def vote(self):
         self.driver.get("https://app.sli.do/event/bsTUZCsBZLNMuByJMnbjhd/live/polls")
-        time.sleep(1.5)
-        click_elem = self.driver.find_element(By.CSS_SELECTOR,"#live-tabpanel-polls > div > div > div > form > div:nth-child(1) > div > div.poll-question__body-container > div > div.poll-question__body > div > label:nth-child(1)")
+        time.sleep(1.7)
+        click_elem = self.driver.find_element(By.CSS_SELECTOR,"#live-tabpanel-polls > div > div > div > form > div:nth-child(1) > div > div.poll-question__body-container > div > div.poll-question__body > div > label:nth-child("+str(self.who)+")")
         click_elem.click()
         time.sleep(.25)
         submit = self.driver.find_element(By.CSS_SELECTOR, "#poll-submit-button")
@@ -25,22 +28,9 @@ class SlidoBot:
 # hash # xpath # times
 def main():
 
-    try:
-        options, args = getopt.getopt(
-            sys.argv[1:], "-d:",
-            ["driver="])
-        for name, value in options:
-            if name in ('-d', '--driver'):
-                print(value)
-                DRIVER = value 
 
-    except getopt.GetoptError as err:
-        print(str(err))
-        print("Invalid args!")
-        sys.exit(1)
-
-    for i in range(1, int(15)+1):
-        BOT = SlidoBot(driver= DRIVER)
+    for i in range(1, int(500)+1):
+        BOT = SlidoBot( who= EVERYONE[i%4])
         BOT.vote()
         BOT.closeBrowser()
         print("Votes: " + str(i))
